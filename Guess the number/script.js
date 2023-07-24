dcbf9bdb11d8351a4cf959d5a434e6ce0096db3d
Prashanth Thiagarajan <prshnth.619@gmail.com>
2023-07-24 21:33:57 +0530

    Adding Guess the number program

---
+++ Guess the number/script.js
@@ -0,0 +1,31 @@
+let gameResult = document.getElementById("gameResult");
+let userInput = document.getElementById("userInput");
+let randomNumber = Math.ceil(Math.random() * 100);
+
+console.log("Current Random number is "+ randomNumber)
+
+function resetRandomNumber(){
+  randomNumber = Math.ceil(Math.random() * 100);
+  console.log("Current Random number is "+ randomNumber)
+}
+
+function checkGuess() {
+  let guessedNumber = parseInt(userInput.value);
+  if (guessedNumber > randomNumber) {
+    gameResult.textContent = "Too High! Try Again.";
+    gameResult.style.backgroundColor = "#1e217c";
+  }
+  else if (guessedNumber < randomNumber) {
+    gameResult.textContent = "Too Low! Try Again.";
+    gameResult.style.backgroundColor = "#1e217c";
+  }
+  else if (guessedNumber === randomNumber) {
+    gameResult.textContent = "Congratulations! You got it right.";
+    gameResult.style.backgroundColor = "green";
+    resetRandomNumber();
+  }
+  else {
+    gameResult.textContent = "Please provide a valid input.";
+    gameResult.style.backgroundColor = "#1e217c";
+  }
+}

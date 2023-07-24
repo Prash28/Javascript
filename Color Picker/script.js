75fd1b09fe82b8a1a18e2b02d43887e5f5ff510e
Prashanth Thiagarajan <prshnth.619@gmail.com>
2023-07-24 22:07:02 +0530

    Adding Color Picker program

---
+++ Color Picker/script.js
@@ -0,0 +1,15 @@
+function clickB1() {
+  document.getElementById("colorPickerContainer").style.backgroundColor = "#e0e0e0"
+}
+
+function clickB2() {
+  document.getElementById("colorPickerContainer").style.backgroundColor = "#6fcf97"
+}
+
+function clickB3() {
+  document.getElementById("colorPickerContainer").style.backgroundColor = "#56ccf2"
+}
+
+function clickB4() {
+  document.getElementById("colorPickerContainer").style.backgroundColor = "#bb6bd9"
+}
